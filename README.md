d4e27f4a199c94fb18892400cfbc8dd0bf9cba80
Hannah <salvadin2@gmail.com>
2025-11-24 01:09:44 -0500

    rewrite with termux workaround, fix readme

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ A command-line APK 'extraction' tool that copies a desired application to /sdcar
 * Install [Termux](https://termux.com/) on your device
 * (This is a one-time step) Run `termux-setup-storage` to allow Termux file access
 * Run `cd /sdcard`
-* (This is a one-time step) Run `curl -O https://suhtiva.github.io/cpapk/cpapk.sh` to download the executable
+* (This is a one-time step) Run `curl -O https://winnpixie.github.io/cpapk/cpapk.sh` to download the executable
 * Run `bash cpapk.sh app_name` (replace `app_name` with the desired application)
 
 ## Notes:
 * You may be required to run `dos2unix` on the `cpapk.sh` executable (`adb shell dos2unix /sdcard/cpapk.sh` or just `dos2unix /sdcard/cpapk.sh` if you're on Termux).
     * I tried to force commits to use LF instead of CRLF however I'm not sure if it worked.
-* Running through Termux on a non-rooted device does not work for any non-System apps and will require an app from the Play Store.
+* Running through Termux on a non-rooted device may not work for non-System apps and could require an app from the Play Store.
     * I can't confirm if this problem persists on a rooted device as I do not own one.
--- cpapk.sh
+++ cpapk.sh
@@ -4,44 +4,47 @@
 # Source: https://github.com/winnpixie/cpapk/
 
 if [[ $1 != "" ]]; then
-    echo "Attempting to copy app '$1' to /sdcard/$1.apk"
+	echo "Attempting to copy app '$1' to /sdcard/$1.apk"
 
-    # Grab package
-    package="$(pm list packages | grep -i $1 | awk -F ':' '{print $2}')"
-    if [[ $package != "" ]]; then
-        echo "Package found: $package"
+	pkg_list="$(pm list packages 2>&1 </dev/null)"
+	parsed_pkg="$(echo "$pkg_list" | grep -i $1 | awk -F ':' '{print $2}')"
+	if [[ $parsed_pkg != "" ]]; then
+		echo "Package found: $parsed_pkg"
 
-        # Grab APK path
-        apk_file="$(pm path $package | awk -F ':' '{print $2}' | grep -i '==/base.apk')"
-        if [[ $apk_file != "" ]]; then
-            echo "APK file found: $apk_file"
+		# Grab APK path
+		apk_path="$(pm path $parsed_pkg 2>&1 </dev/null)"
+		apk_file="$(echo "$apk_path" | awk -F ':' '{print $2}' | grep -i '==/base.apk')"
+		if [[ $apk_file != "" ]]; then
+			echo "APK file found: $apk_file"
 
-            # Copy APK file to /sdcard
-            echo "Copying $apk_file to /sdcard/$1.apk"
-            cp $apk_file "/sdcard/$1.apk"
+			# Copy APK file to /sdcard
+			echo "Copying $apk_file to /sdcard/$1.apk"
+			cp $apk_file "/sdcard/$1.apk"
 
-            echo "Copy successful"
-        else
-            echo "APK file could not be located"
-        fi
-    else
-        echo "Could not find package for $1, searching through System apps"
+			echo "Copy successful"
+			return 0 2>/dev/null || exit 0
+		fi
+	fi
 
-        # Grab directory from /system/app
-        apk_file="$(ls /system/app | grep -i $1)"
-        if [[ $apk_file != "" ]]; then
-            echo "System APK file found: $apk_file.apk"
+	echo "Could not find package for '$1', searching System apps"
 
-            # Copy APK file to /sdcard
-            echo "Copying /system/app/$apk_file/$apk_file.apk to /sdcard/$apk_file.apk"
-            cp /system/app/$apk_file/$apk_file.apk /sdcard/
+	# Test system paths
+	for sys_path in "/system/app" "/system/priv-app" "/system_ext/app" "/system_ext/priv-app"; do
+		apk_file="$(ls $sys_path | grep -i $1)"
 
-            echo "Copy successful"
-        else
-            echo "System APK file could not be located"
-        fi
-    fi
-    echo "Attempt completed"
+		if [[ $apk_file != "" ]]; then
+			echo "System app '$apk_file.apk' found in $sys_path"
+			
+			# Copy APK to /sdcard/
+			echo "Copying $sys_path/$apk_file/$apk_file.apk to /sdcard/$apk_file.apk"
+			cp $sys_path/$apk_file/$apk_file.apk /sdcard/$apk_file.apk
+
+			echo "Copy successful"
+			return 0 2>/dev/null || exit 0
+		fi
+	done
+
+	echo "Could not find System app, are you sure '$1' exists?"
 else
-    echo "usage: cpapk app_name"
+	echo "usage: cpapk app_name"
 fi
